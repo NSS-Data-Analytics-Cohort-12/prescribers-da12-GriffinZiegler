f1a968529983a058cce035dc1f0066d17e7b7f29
Griffin Ziegler <griffintyziegler@gmail.com>
2024-06-13 20:03:36 -0400

    Question 1b MVP

--- ANSWERS.sql
+++ ANSWERS.sql
@@ -5,15 +5,23 @@
 -- 1. 
 --     a. Which prescriber had the highest total number of claims (totaled over all drugs)? Report the npi and the total number of claims.
 
-SELECT nppes_provider_first_name AS first_name, nppes_provider_last_org_name AS last_name, npi, total_claim_count
+SELECT npi, total_claim_count
 FROM prescription
 JOIN prescriber USING(npi)
 ORDER BY total_claim_count DESC;
 
 --     b. Repeat the above, but this time report the nppes_provider_first_name, nppes_provider_last_org_name,  specialty_description, and the total number of claims.
 
+SELECT nppes_provider_first_name AS first_name, nppes_provider_last_org_name AS last_name, specialty_description, npi, total_claim_count
+FROM prescription
+JOIN prescriber USING(npi)
+ORDER BY total_claim_count DESC;
+
 -- 2. 
 --     a. Which specialty had the most total number of claims (totaled over all drugs)?
+
+
+
 
 --     b. Which specialty had the most total number of claims for opioids?
 
